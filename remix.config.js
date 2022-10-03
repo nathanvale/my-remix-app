c51e3ee2890ee243e6243d043dc57a46aa273fc5
Nathan Vale <nathan.vale@auspost.com.au>
2022-10-04 09:57:55 +1100

    add netlify config

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,14 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: "netlify",
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
+  // publicPath: "/build/",
 };
